a1e9a853d9c0d46f142b403766cc3061d88721c4
thiagowaib <thiagowaib@gmail.com>
2025-10-01 20:48:43 -0300

    Ping Endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { DrizzleModule } from './db/drizzle.module';
-import { LogioService } from './logio/logio.service';
 import { LogioMiddleware } from './logio/logio.middleware';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { LogioService } from './logio/logio.service';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [DrizzleModule],
-  controllers: [],
-  providers: [LogioService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DrizzleModule
+  ],
+  controllers: [AppController],
+  providers: [AppService, LogioService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
